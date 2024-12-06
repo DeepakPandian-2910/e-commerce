d0dd7afdfaee4735e5a3133d7aee82a8d36077b1
unknown <deepakpandian2910@gmail.com>
2024-12-07 01:08:38 +0530

    hi

--- src/main/java/com/example/ecommerce/controller/CategoryController.java
+++ src/main/java/com/example/ecommerce/controller/CategoryController.java
@@ -1,4 +1,25 @@
 package com.example.ecommerce.controller;
 
-public class categoryController {
+import com.example.ecommerce.model.Category;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@RestController
+public class CategoryController {
+    List<Category> categoryList = new ArrayList<>();
+
+    @PostMapping("/api/admin/category")
+    public void addCategory (@RequestBody Category category) {
+        categoryList.add(category);
+    }
+
+    @GetMapping("api/public/categories")
+    public  List<Category> getAllCategories() {
+        return categoryList;
+    }
 }
